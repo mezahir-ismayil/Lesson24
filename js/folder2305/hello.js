d848866419caccf4ed8ee12c62e102c3a072bb9c
mezahir-ismayil <mezahir.ismayilzade@mail.ru>
2024-06-22 17:39:45 +0400

    first commit

---
+++ js/folder2305/hello.js
@@ -0,0 +1,635 @@
+// let h1 = document.querySelector("h1:nth-child(3)");
+// let h1 = document.querySelectorAll("h1");
+// let array = ["Axmed",3,2,34,56,56];
+// Helloworld helloworld5
+// console.log(h1);
+
+// let p = document.createElement("p");
+// let div = document.querySelector(".parent");
+// let span = document.querySelector("span");
+// // div.append(p);
+// // div.insertBefore(p,span);
+// // p.remove();
+// // span.remove();
+
+// let h1 = document.querySelector("h1");
+// let p1 = document.querySelector("p:nth-child(1)");
+// let p2 = document.querySelector("p:nth-child(2)");
+// // h1.remove();
+// // console.log(h1.innerHTML);
+// // console.log(h1.innerText);
+// console.log(p2.innerText);
+// console.log(p2.innerHTML);
+// console.log(p2.textContent);
+// console.log(div.innerHTML);
+// console.log(div.innerText);
+// console.log(div.textContent);
+
+
+// let p = document.createElement("p");
+// let body = document.querySelector("body");
+// p.innerText = "Hello Emin";
+// body.append(p);
+
+
+
+// let element1 = document.querySelector("li:nth-child(1)");
+// console.log(element1);
+
+
+// let p1 = document.querySelector(".p1");
+// // p1.innerHTML = "<span>Hello</span>";
+// console.log(p1.innerHTML);
+// console.log(p1.innerText);
+
+// let li = document.querySelector("li:first-child");
+// li.remove();
+// let newli = document.createElement("li");
+// newli.innerHTML = "list 5";
+// let ul = document.querySelector("ul");
+// let li4 = document.querySelector(".new");
+// ul.insertBefore(newli, li4);
+
+
+// let p1 = document.querySelector("p");
+// let p1 = document.getElementById("p11");
+// let p1 = document.getElementsByClassName("p1");
+// let p1 = document.getElementsByTagName("p")[0];
+// p1.style.backgroundColor = "red";
+// p1.style.color = "white";
+// p1.style.margin = "10px";
+// console.log(p1);
+
+// let p = document.querySelector(".p");
+// let p = document.createElement("p");
+// let p2 = document.createElement("p");
+// le
+// p2.innerHTML = "jvnflvf"
+// let div = document.querySelector("div");
+// // let text = "Hello World"; 
+// // p.appendChild(text);
+// div.append(p2);
+
+// let tip = document.title;
+// console.log(tip);
+
+
+
+
+// let allLi = document.querySelectorAll("li");
+
+// for(let i=0; i < allLi.length; i++) {
+//     if (i==1 || i==3) {
+//         continue;
+//     } else {
+//         allLi[i].style.color = "red";
+//     }
+// }
+
+// let a = document.querySelector("a");
+// a.href = "https://meet.google.com/";
+// a.id = "bye bye";
+// let div = document.querySelector("div");
+// div.setAttribute("id", "id1");
+// let newP = document.createElement("p");
+// document.appendChild(newP);
+// console.log(div);
+
+// let allA = document.baseURI;
+// let body = document.body;
+// console.log(body);
+
+// let div = document.querySelectorAll("div")[1];
+// // console.log(div);
+// // div.remove();
+// let img = document.
+// for(i=0; i<5; i++) {
+//     if (i==1 || i==3) {
+//         continue;
+//     } 
+// }
+// let newP = document.createElement("p");
+
+// document.appendChild(newP);
+
+// let div = document.getElementsByClassName("div1")[0];
+// console.log(div);
+// let div = document.getElementsByTagName("div")[2];
+// console.log(div);
+
+// let ul = document.createElement("ul");
+// // let h1 = document.createElement("h1");
+// // let span = document.querySelector("span");
+// let div = document.getElementsByClassName("div1")[0];
+// // console.log(span.textContent);
+// p.textContent = "Hello World"; 
+// // p.append(document.createElement("span").textContent = "asdfg");
+// // p.innerHTML = "<span>SPAN</span> <h1>Hello World</h1>";
+// p.innerHTML = "<p>sdfgh</p>";
+// console.log(p);
+// div.append(p);
+// ul.innerHTML = "<>";
+// const car = {
+//     type:"Fiat",
+//     // model:"500", 
+//     color:"white"
+// };
+// console.log(car);
+// let div1 = document.createElement("div");
+// let divpar = document.querySelector(".parent");
+// let li = document.createElement("li");
+// let li1 = document.createElement("li");
+// let li2 = document.createElement("li");
+// ul.append(li,li1,li2)
+// div.append(ul)
+// div.innerHTML = "<ul> <li>list1</li>  <li>list2</li>  <li>list3</li></ul>";
+
+// let b = prompt("color");
+// div1.style.backgroundColor = b;
+// divpar.append(div1);
+// console.log(b);
+
+// let a = "Hello";
+// console.log(a[1]);
+// console.log("ə");
+// let parent = document.querySelector(".parent");
+
+
+// document.querySelector(".parent").innerHTML = `<div style = 'background-color: ${prompt("Color:")}'></div>`;
+// console.log('Hello');
+
+// document.embeds
+// let forms = document.forms;
+// let head = document.head;
+// let img = document.images[0];
+// img.src = "https://img.etimg.com/thumb/width-1200,height-900,imgsize-74560,resizemode-75,msid-104220582/top-trending-products/electronics/laptops/best-gaming-computer-sets-for-an-unparalleled-experience-starting-at-just-23999.jpg";
+// let dom = document.implementation;
+// let code = document.inputEncoding;
+
+
+// let date = document.title;
+// console.log(date);
+// console.log(Date());
+// document.write("lorem")
+
+// let form = document.querySelector("form");
+// let inp = document.querySelector("input");
+// let array  = [];
+// array.push(form.children);
+// array.push(form.childNodes);
+// for (let i = 0; i < array.length; i++) {
+    // array[i] = toString();   
+    // array1.push(array[].toString());
+// }
+// let inp = document.querySelector("input");
+// let array = [2,3,4,6,7,8]
+// console.log(array.includes(document.nodeName = "input"));
+// if (array.includes(array[0]= "input")) {
+    // inp.value = "Hello";
+// }
+// if (form.children = "input") {
+    // inp.value = "Hello";
+// }
+// console.log(array.includes(array[0]= "a"));
+// console.log(form.children = "input");
+// let a = 5;
+// console.log(a.toString());
+
+
+
+
+// let form = document.querySelector("form");
+// let inp = document.querySelector("input");
+// let array = [];
+// array.push(form.children);
+// console.log(form.children);
+// console.log(array);
+// console.log(array[0][2]);
+// if (array.includes(array[0]= "input")) {
+    // inp[0].value = "Ali";
+    // inp[1].value = "Ali";
+// }
+
+
+// let inp = document.querySelectorAll("form a");
+
+// console.log(inp);
+
+// console.log(inp);
+// if (inp.length!=0) {
+//     for (let i = 0; i < inp.length; i++) {
+//         inp[i].value = "Hello world";
+//     }
+// }
+
+// узел
+
+// let form = document.querySelector("form");
+// let input = document.querySelector("input");
+// console.log(form.lastChild.length);
+// console.log(form.children.length);
+
+
+// let form = document.getElementsByTagName("form")[0];
+// let a = document.getElementsByTagName("a")[0];
+// console.log(a.previousSibling);
+
+// console.log(form[0].parentNode);
+// console.log(form.childNodes[form.childNodes.length - 1]);
+// console.log(form.childNodes[10]);
+// console.log(form.lastChild);
+
+
+
+// let img = document.getElementsByTagName("img")[0];
+// img.setAttribute("src", "");
+// let form = document.getElementsByTagName("form")[0];
+// let p = document.getElementsByTagName("p")[0];
+// let sp = document.createElement("span");
+// p.innerHTML = "Hello";
+// form.appendChild(p);
+// form.removeChild(p);
+// p.remove();
+// form.replaceChild(sp, p);
+// sp.innerHTML = "Hello";
+// let a = 500;
+// document.write(a);
+// p.childNodes[1].nodeValue = "Hi";
+// console.log(form.childNodes[3]);
+
+// console.log(form.childNodes[1].nodeValue);
+// console.log(p.nodeValue);
+// console.log(document.getElementById("id1").childNodes[0].nodeValue);
+
+// let form = document.getElementsByTagName("form")[0];
+// console.log(form.childNodes[1].childNodes[0].nodeValue[0]);
+// console.log(a);
+// a.forEach(element => {
+    // element.innerHTML = "Set the note";
+    // element.childNodes[0]
+    // element.childNodes[0].nodeValue = "Set the note";
+    // console.log();
+// });
+// console.log(a);
+// console.log(form.childNodes[1].childNodes[0].nodeValue[0]);
+// let a = document.querySelectorAll("a");
+// a.forEach(element => {
+    // element.childNodes[0].nodeValue = "Set the Note";
+// });
+
+
+// function newFunc() {
+    // document.getElementById("p").innerHTML = "Hi guys";
+    // console.log("hello");
+    // alert("Good bye");
+    // prompt("Why are you leaving us");
+    // console.log("Hi");
+// }
+// alert("hi ");
+// let p =document.querySelector(".p1");
+// 
+// function sigma() {
+    // p.innerHTML++;
+// }
+// 
+
+// let p2 =document.querySelector(".p2")
+
+// function sigma2() {
+    // p2.innerHTML--;
+// }
+
+// let x = 0;
+// x=x+1;
+// console.log(x);
+
+
+
+
+// console.log(x);
+// function func1() {
+//     let x = document.getElementById("sel1").value;
+// document.querySelector("p").innerHTML = "Вы изменили значение на " + x;
+// }
+
+// let p = document.querySelector("p");
+// // function mouse() {
+// //     p.style.color = "red";
+// // }
+// // function mouse2() {
+// //     p.style.color = "green";
+// // }
+
+// function mouse1() {
+//     p.innerHTML = "Ali";
+    
+// }
+// function mouse2() {
+//     p.innerHTML = "Huseyn";
+
+
+
+// let div = document.querySelector("div");
+// let select = document.querySelector("select");
+// let button = document.getElementById("btn1");
+// select.addEventListener("change", myfunction1);
+
+// function myfunction1() {
+//     document.getElementsByTagName("img")[0].style.display = "none";
+//     document.getElementsByTagName("img")[1].style.display = "none";
+//     document.getElementsByTagName("img")[2].style.display = "none";
+//     let x = document.getElementById("select").value;
+//     console.log(x);
+//     div.append(document.getElementById(x));
+//     document.getElementById(x).style.display = "block";
+
+//     // document.write("Hello");
+// }
+
+
+// let div = document.querySelector("div");
+// let a = 0;
+// let p = document.querySelector("p");
+// // let button = document.querySelector("button");
+
+// div.addEventListener("mousemove", myfunction2);
+// // button.addEventListener("click", functionRemove);
+
+
+// function myfunction2() {
+//     // p.innerHTML = Math.round(Math.random()*100);
+//     p.innerHTML = event.clientX;
+// // console.log(MouseEvent.clientX);
+// }
+// function functionRemove() {
+    // div.removeEventListener("mousemove", myfunction2);
+// }
+
+
+// console.log(Math.round(Math.random()*100));
+
+
+
+//event bubbling
+// event capturing
+
+// let button = document.querySelector("button");
+// button.addEventListener("click", myfunction1);
+// // button.onclick = myfunction1;
+
+// function myfunction1() {
+//     document.getElementById("p").innerHTML = "Clicked";
+// }
+
+// let div = document.querySelector("div");
+// console.log(div.childNodes[1].childNodes[0].nodeValue.length);
+
+
+// let div = document.querySelector("div");
+// let p = document.getElementById("p");
+// let p1 = document.getElementById("p1");
+// let button = document.querySelector("button");
+// let input = document.querySelector("input");
+
+// button.addEventListener("click", function1);
+// // input.addEventListener("focus", function1);
+// p1.addEventListener("click", function2);
+// div.addEventListener("click", function3);
+
+
+// function function1() {
+    
+//     alert("btn " + event.eventPhase);
+//     // event.stopPropagation();
+// }
+
+// function function2() {
+//     alert("p " + event.eventPhase);
+// }
+
+// function function3() {
+//     alert("div " + event.eventPhase);
+// }
+
+// let blue = document.getElementById("blue");
+// let back = document.getElementById("back");
+// blue.addEventListener("mousemove", biggerDiv);
+
+// let width = 10;
+// function biggerDiv() {
+
+//     back.style.width = `${width}px`;
+//     if(width > 300) {
+ 
+//     } else {
+//         width++;
+//     }
+// }
+
+// blue.innerHTML = `${width*3}/3`;
+
+
+// let p = document.getElementById("text");
+// let btn = document.querySelector("button");
+// btn.addEventListener("click", stopTimeout)
+// let timeout = setInterval(writeText, 1000);
+// // writeText();
+
+
+// function writeText() {
+//     let date = new Date();
+//     p.innerHTML = date.toLocaleString();
+// }
+
+// function stopTimeout() {
+//     clearInterval(timeout);
+// }
+
+// let today, someday, text;
+// today = new Date().getDay();
+// someday = new Date();
+// today.getTime();
+
+// someday.setFullYear(2024, 5, 12);
+// document.getElementById("text").innerHTML = today;
+// let interval = setInterval(time, 1000);
+
+// function time() {
+//     let date = new Date();
+//     p.innerHTML = date.toLocaleString();
+// }
+// time();
+
+
+// let p = document.getElementById("text");
+// let first = document.getElementById("first");
+// let sec = document.getElementById("sec");
+// let last = document.getElementById("last");
+// let btn1 = document.getElementById("start");
+// let btn2 = document.getElementById("stop");
+
+// let second = sec.innerHTML;
+// let minutes = first.innerHTML;
+// let millis = last.innerHTML;
+
+// btn1.addEventListener("click", startsec);
+// btn2.addEventListener("click", stopsec);
+
+// let interval1;
+// function startsec() {
+//     interval1 = setInterval(secundomer, 1);
+// }
+// function stopsec() {
+//     clearInterval(interval1);
+// }
+
+// function secundomer() {
+//     last.innerHTML = millis;
+//     sec.innerHTML = second;
+//     first.innerHTML = minutes;
+//     // second++;
+//     millis++;
+//     if(millis >= 100) {
+//         millis=0;
+//         second++;
+//     }
+//     if(second >= 60) {
+//         second=0;
+//         minutes++;
+//     }
+//     if(second < 10 && second > 0) {
+//         sec.innerHTML = "0" + second;
+//     }
+//     if(minutes < 10 && minutes > 0) {
+//         first.innerHTML = "0" + minutes;
+//     }
+// }
+
+// let date = new Date();
+// let p = document.getElementById("p");
+
+// p.innerHTML = date.getFullYear();
+// p.innerHTML = date.getHours();
+// p.innerHTML = date.getMonth();
+// p.innerHTML = date.getMilliseconds();
+// date.setFullYear(2024, 5, 12);
+// p.innerHTML = date.getMonth() + 1;
+
+
+// let input = document.getElementsByTagName("input")[0];
+
+// let interval;
+
+// function timer() {
+    //     p.innerHTML = input.value;    
+    //     interval = setInterval(function()  {
+        //         p.innerHTML--;
+        //         if (p.innerHTML <= 0) {
+            //             clearInterval(interval);
+            //         }
+            //     }, 1000)
+            // }
+            
+// let p = document.getElementsByTagName("p")[0];
+// let p2 = document.getElementsByTagName("p")[1];
+// let img = document.getElementsByTagName("img")[0];
+
+// img.src = "https://www.frommers.com/system/media_items/attachments/000/854/387/s980/from_top_of_the_rock__ryan_flicker.com.jpg?1423068667";
+// p.classList.add("huseyn");
+// p.classList.add("huseyn1");
+// p.classList.add("huseyn2");
+// p.classList.add("huseyn3");
+// p.classList.add("huseyn4");
+// p.classList.add("huseyn5");
+// p.classList.add("huseyn61");
+// p.classList.remove("huseyn");
+// p.classList.toggle("huseyn");
+
+// p.className = "ali ali2";
+
+// let img = document.querySelectorAll("img");
+
+// function allImages() {
+//     console.log("sdfghjn");
+//     img.forEach(images => images.style.display = "block")
+// }
+
+// function hello() {
+//     console.log("dfgh");
+// }
+// console.log("Sdfg");
+
+
+// let div = document.querySelector("div");
+// let btn = document.querySelector("button");
+// let a = 0;
+
+// function clickMe() {
+//     a++;
+//     if (a == 1) {
+//         div.style.backgroundColor = "red";
+//     } else if (a == 2) {
+//         div.style.backgroundColor = "blue";
+//     }
+//     else if (a == 3) {
+//         div.style.backgroundColor = "green";    
+//     }
+//     else if (a == 4) {
+//         div.style.backgroundColor = "transparent";    
+//     }
+//     else {
+//         a = 0;
+//     }
+// } 
+
+// let btn = document.querySelector("button");
+// let img = document.querySelectorAll("img");
+// let i = -1300;
+
+// function clock() {
+//         let intenal = setInterval(() => {
+//                 img.forEach(element => {
+//                 element.style.left = `${i}px`;    
+//                 });
+                
+//                 i+=2;
+//                 if (i >= 500) {
+//                     i = -1300;
+//                 }
+//             }, 10);
+// }
+
+
+
+// let btn = document.querySelector("button");
+// let img = document.querySelectorAll("img");
+// let i = -1450;
+
+// function anime() {
+//     let interval = setInterval(() => {
+//         img.forEach(element => {
+//             element.style.left = `${i}px`;
+
+//         })
+//         i+=1;
+//         if (i>=500) {
+//             i=-1450;
+//         }
+//     }, 1)    
+// }
+
+let text = "10 100 1000 110000";
+// let n = text.search(/l/i);
+// let  n = text.replace(/hello/i, "Bye bye")
+// let n  = text.match(/W/i);
+// let n = text.match(/[^1-4]/g);
+// let n = text.match(/(red|green)/g);
+// let n = text.match(/\s/g);
+// let n = text.search(/65\b/)
+// let n = text.match(/\u0041/ig);
+// let n = text.match(/10?/g);
+let n = /a/.test("jysudgfuibhjoeff")
+
+console.log(n);
+// console.log(3**2);
+// console.log("Hello \n World");
